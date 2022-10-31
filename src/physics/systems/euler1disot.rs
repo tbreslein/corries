50a23c070229dca68f1280234bdb2f73d621ba0d
tbreslein <tommy.breslein@protonmail.com>
2022-10-31 20:31:41 +0100

    make Physics generic over dimensions

--- src/physics/systems/euler1disot.rs
+++ src/physics/systems/euler1disot.rs
@@ -2,38 +2,36 @@
 // Author: Tommy Breslein (github.com/tbreslein)
 // License: MIT
 
-use color_eyre::Result;
 use ndarray::Axis;
 
 use crate::{
     config::physicsconfig::PhysicsConfig,
-    mesh::Mesh,
     physics::{variables::Variables, Physics},
     units::Units,
 };
 
-pub struct Euler1DIsot {
-    pub cent: Variables,
+pub struct Euler1DIsot<const S: usize, const EQ: usize> {
+    pub cent: Variables<S, EQ>,
     pub units: Units,
 }
 
-impl Euler1DIsot {
-    pub fn new(physicsconf: &PhysicsConfig, mesh: &Mesh) -> Result<Box<dyn Physics>> {
-        let cent = Variables::new(physicsconf, mesh);
+impl<const S: usize, const EQ: usize> Euler1DIsot<S, EQ> {
+    pub fn new(physicsconf: &PhysicsConfig) -> Self {
+        let cent = Variables::new(physicsconf);
         let units = Units::new(physicsconf.units_mode);
-        return Ok(Box::new(Euler1DIsot { cent, units }));
+        return Euler1DIsot { cent, units };
     }
 }
 
-impl Physics for Euler1DIsot {
-    fn update_cons(&self, vars: &mut Variables) {
+impl<const S: usize, const EQ: usize> Physics<S, EQ> for Euler1DIsot<S, EQ> {
+    fn update_cons(&self, vars: &mut Variables<S, EQ>) {
         vars.c.index_axis_mut(Axis(0), 0).assign(&vars.p.index_axis(Axis(0), 0));
         vars.c
             .index_axis_mut(Axis(0), 1)
             .assign(&(&vars.p.index_axis(Axis(0), 1) * &vars.p.index_axis(Axis(0), 0)));
     }
 
-    fn update_prim(&self, vars: &mut Variables) {
+    fn update_prim(&self, vars: &mut Variables<S, EQ>) {
         vars.p.index_axis_mut(Axis(0), 0).assign(&vars.c.index_axis(Axis(0), 0));
         vars.p
             .index_axis_mut(Axis(0), 1)
