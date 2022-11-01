5e04d6cd809de2e11e1179dffcac4bf4db99008b
tbreslein <tommy.breslein@protonmail.com>
2022-11-01 10:15:08 +0100

    fix Euler1DIsot

--- src/physics.rs
+++ src/physics.rs
@@ -9,7 +9,6 @@ use crate::config::physicsconfig::{PhysicsConfig, PhysicsMode};
 use self::systems::euler1disot::Euler1DIsot;
 
 mod systems;
-pub mod variables;
 
 pub trait Physics<const S: usize, const EQ: usize> {
     fn update_cons(&mut self);
@@ -17,12 +16,15 @@ pub trait Physics<const S: usize, const EQ: usize> {
 
     fn assign_prim(&mut self, i: usize, j: usize, x: f64);
     fn assign_cons(&mut self, i: usize, j: usize, x: f64);
+    fn assign_c_sound(&mut self, i: usize, x: f64);
 
     fn assign_prim_vec(&mut self, j: usize, x: &Array1<f64>);
     fn assign_cons_vec(&mut self, j: usize, x: &Array1<f64>);
+    fn assign_c_sound_vec(&mut self, x: &Array1<f64>);
 
     fn get_prim(&self, i: usize, j: usize) -> f64;
     fn get_cons(&self, i: usize, j: usize) -> f64;
+    fn get_c_sound(&self, i: usize) -> f64;
 }
 
 pub fn init_physics<const S: usize, const EQ: usize>(physicsconf: &PhysicsConfig) -> Box<dyn Physics<S, EQ>> {
