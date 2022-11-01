7af4497e3f9191612cd8dad802f11b4cc5618827
tbreslein <tommy.breslein@protonmail.com>
2022-11-01 22:30:53 +0100

    add physical flux and eigenvalue methods for Physics

--- src/physics/systems/euler1disot.rs
+++ src/physics/systems/euler1disot.rs
@@ -1,3 +1,5 @@
+use ndarray::{azip, Array2};
+
 use super::super::Physics;
 
 impl<const S: usize, const EQ: usize> Physics<S, EQ> {
@@ -5,19 +7,19 @@ impl<const S: usize, const EQ: usize> Physics<S, EQ> {
     #[inline(always)]
     pub fn update_cons_euler1d_isot(&mut self) {
         self.cons.row_mut(self.jdensity).assign(&self.prim.row(self.jdensity));
-        self.calc_cons_linear_momentum_euler(self.jximomentum);
+        self.update_cons_linear_momentum_euler(self.jximomentum);
     }
 
     /// Updates primitive density and xi velocity according to the isothermal Euler equations
     #[inline(always)]
     pub fn update_prim_euler1d_isot(&mut self) {
         self.prim.row_mut(self.jdensity).assign(&self.cons.row(self.jdensity));
-        self.calc_prim_linear_velocity_euler(self.jxivelocity);
+        self.update_prim_linear_velocity_euler(self.jxivelocity);
     }
 
     /// Calculates and updates the linear momentum in [self.cons] at row `j`.
     #[inline(always)]
-    pub fn calc_cons_linear_momentum_euler(&mut self, j: usize) {
+    pub fn update_cons_linear_momentum_euler(&mut self, j: usize) {
         self.cons
             .row_mut(j)
             .assign(&(&self.prim.row(j) * &self.prim.row(self.jdensity)));
@@ -25,7 +27,7 @@ impl<const S: usize, const EQ: usize> Physics<S, EQ> {
 
     /// Calculates and updates the linear velocity in [self.prim] at row `j`.
     #[inline(always)]
-    pub fn calc_prim_linear_velocity_euler(&mut self, j: usize) {
+    pub fn update_prim_linear_velocity_euler(&mut self, j: usize) {
         self.prim
             .row_mut(j)
             .assign(&(&self.cons.row(j) / &self.cons.row(self.jdensity)));
@@ -38,5 +40,24 @@ impl<const S: usize, const EQ: usize> Physics<S, EQ> {
             .assign(&(self.adiabatic_index * &self.prim.row(self.jpressure) / &self.prim.row(self.jdensity)));
         self.c_sound_helper.mapv_inplace(f64::sqrt);
         self.c_sound.assign(&self.c_sound_helper);
+    }
+
+    /// Calculates the physical flux for isothermal 1D Euler
+    #[inline(always)]
+    pub fn calc_physical_flux_euler1d_isot(&self, flux: &mut Array2<f64>) {
+        self.calc_density_flux_euler1d_isot(flux);
+        self.calc_xi_momentum_flux_euler1d_isot(flux);
+    }
+
+    /// Calculates the density flux for isothermal 1D Euler
+    #[inline(always)]
+    pub fn calc_density_flux_euler1d_isot(&self, flux: &mut Array2<f64>) {
+        azip!((dens_flux in flux.row_mut(self.jdensity), &ximom in self.cons.row(self.jximomentum)) *dens_flux = ximom);
+    }
+
+    /// Calculates the xi momentum flux for isothermal 1D Euler
+    #[inline(always)]
+    pub fn calc_xi_momentum_flux_euler1d_isot(&self, flux: &mut Array2<f64>) {
+        azip!((ximom_flux in flux.row_mut(self.jximomentum), &dens in self.cons.row(self.jdensity), &xivel in self.prim.row(self.jxivelocity), &cs in &self.c_sound) *ximom_flux = dens * (xivel * xivel + cs * cs));
     }
 }
