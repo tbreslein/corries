9d18f7615517181c92463d9917b13cbbbb520602
tbreslein <tommy.breslein@protonmail.com>
2022-11-03 21:26:33 +0100

    add missing docs for rhs

--- src/rhs.rs
+++ src/rhs.rs
@@ -19,7 +19,7 @@ pub struct Rhs<'a, const S: usize, const EQ: usize> {
     pub full_rhs: Array2<f64>,
 
     /// Calculates the numerical flux
-    numflux: &'a mut dyn numflux::NumFlux<S, EQ>,
+    numflux: &'a mut dyn NumFlux<S, EQ>,
 
     /// Stores the numerical flux derivative along xi
     dflux_dxi: Array2<f64>,
@@ -30,6 +30,12 @@ pub struct Rhs<'a, const S: usize, const EQ: usize> {
 
 impl<'a, const S: usize, const EQ: usize> Rhs<'a, S, EQ> {
     /// Constructs a new [Rhs] object.
+    ///
+    /// # Arguments
+    ///
+    /// * `config` - Configuration for the whole simulation
+    /// * `u` - The main [Physics] object for this simulation
+    /// * `numflux` - The [dyn NumFlux] object about to be stored in this [Rhs] object
     pub fn new(config: &CorriesConfig, u: &Physics<S, EQ>, numflux: &'a mut dyn NumFlux<S, EQ>) -> Self {
         return Rhs {
             full_rhs: Array2::zeros((EQ, S)),
@@ -39,11 +45,23 @@ impl<'a, const S: usize, const EQ: usize> Rhs<'a, S, EQ> {
         };
     }
 
+    /// Solves the right-hand side and updates the `full_rhs` field
+    ///
+    /// # Arguments
+    ///
+    /// * `u` - The current [Physics] state
+    /// * `mesh` - Information about spatial properties
     pub fn update(&mut self, u: &mut Physics<S, EQ>, mesh: &Mesh<S>) {
         self.update_dflux_dxi(u, mesh);
         self.full_rhs.assign(&self.dflux_dxi);
     }
 
+    /// Updates the numerical flux derivative `dflux_dxi`
+    ///
+    /// # Arguments
+    ///
+    /// * `u` - The current [Physics] state
+    /// * `mesh` - Information about spatial properties
     fn update_dflux_dxi(&mut self, u: &mut Physics<S, EQ>, mesh: &Mesh<S>) {
         // this assumes that u.cons is up-to-date; self.update_physics updates u.prim and the
         // derived variables anyways.
@@ -51,6 +69,13 @@ impl<'a, const S: usize, const EQ: usize> Rhs<'a, S, EQ> {
         self.numflux.calc_dflux_dxi(&mut self.dflux_dxi, u, mesh);
     }
 
+    /// Calls different methods of `u` to update everything, assuming the conservative variables
+    /// are up-to-date.
+    ///
+    /// # Arguments
+    ///
+    /// * `u` - The current [Physics] state about to be updated
+    /// * `mesh` - Information about spatial properties
     pub fn update_physics(&mut self, u: &mut Physics<S, EQ>, mesh: &Mesh<S>) {
         u.update_prim();
         u.update_derived_variables();
