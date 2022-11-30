ac2186737129b8632b8b09d9a460462b44b2f95e
tbreslein <tommy.breslein@protonmail.com>
2022-12-01 00:11:48 +0100

    add some more utility methods ++ methods for updating eigen_vals and physical flux

--- src/physics.rs
+++ src/physics.rs
@@ -10,35 +10,66 @@ pub mod systems;
 
 /// Trait for Physics objects
 pub trait Physics {
-    /// Return copy of the primitive variable in equation j at index i
+    /// Return copy of the primitive variable in row j at column i
     fn prim_entry(&self, j: usize, i: usize) -> f64;
 
-    /// Return a view of the equation j of the primitive variables
+    /// Return a view of the row j of the primitive variables
     fn prim_row(&self, j: usize) -> ArrayView1<f64>;
 
     /// Return a view of primitive variables
     fn prim(&self) -> ArrayView2<f64>;
 
-    /// Assign rhs to primitive variables
-    fn assign_prim(&mut self, rhs: &Array2<f64>);
-
-    /// Return copy of the conservative variable in equation j at index i
+    /// Return copy of the conservative variable in row j at column i
     fn cons_entry(&self, j: usize, i: usize) -> f64;
 
-    /// Return a view of the equation j of the conservative variables
+    /// Return a view of the row j of the conservative variables
     fn cons_row(&self, j: usize) -> ArrayView1<f64>;
 
     /// Return a view of conservative variables
     fn cons(&self) -> ArrayView2<f64>;
 
-    /// Assign rhs to conservative variables
-    fn assign_cons(&mut self, rhs: &Array2<f64>);
+    /// Return a view of eigen value matrix
+    fn eigen_vals(&self) -> ArrayView2<f64>;
+
+    /// Return a view of the vector of minimal eigen values
+    fn eigen_min(&self) -> ArrayView1<f64>;
+
+    /// Return a view of the vector of maximal eigen values
+    fn eigen_max(&self) -> ArrayView1<f64>;
+
+    /// Return a view of speed of sound vector
+    fn c_sound(&self) -> ArrayView1<f64>;
+
+    /// Return copy of the physical flux in row j at column i
+    fn flux_entry(&self, j: usize, i: usize) -> f64;
+
+    /// Return a view of the row j of the physical flux
+    fn flux_row(&self, j: usize) -> ArrayView1<f64>;
+
+    /// Return a view of the physical flux
+    fn flux(&self) -> ArrayView2<f64>;
 
     /// Update primitive variables
     fn update_prim(&mut self);
 
     /// Update conservative variables
     fn update_cons(&mut self);
+
+    /// Update values not part of the primitive and conservative variables, i.e. speed of sound,
+    /// eigen values, physical flux and maybe others
+    fn update_derived_values(&mut self);
+
+    /// Assign rhs to primitive variables at row j and column i
+    fn assign_prim_entry(&mut self, rhs: f64, j: usize, i: usize);
+
+    /// Assign rhs to primitive variables
+    fn assign_prim(&mut self, rhs: &Array2<f64>);
+
+    /// Assign rhs to primitive variables at row j and column i
+    fn assign_cons_entry(&mut self, rhs: f64, j: usize, i: usize);
+
+    /// Assign rhs to conservative variables
+    fn assign_cons(&mut self, rhs: &Array2<f64>);
 }
 
 #[cfg(test)]
--- src/physics/systems/euler1disot.rs
+++ src/physics/systems/euler1disot.rs
@@ -22,6 +22,9 @@ pub struct Euler1DIsot<const S: usize> {
 
     /// Eigen values
     pub eigen_vals: Array2<f64>,
+
+    /// Physical flux
+    pub flux: Array2<f64>,
 }
 
 impl<const S: usize> Euler1DIsot<S> {
@@ -32,6 +35,7 @@ impl<const S: usize> Euler1DIsot<S> {
             cons: Array2::zeros((2, S)),
             c_sound: Array1::zeros(S),
             eigen_vals: Array2::zeros((2, S)),
+            flux: Array2::zeros((2, S)),
         };
     }
 }
@@ -53,11 +57,6 @@ impl<const S: usize> Physics for Euler1DIsot<S> {
     }
 
     #[inline(always)]
-    fn assign_prim(&mut self, rhs: &Array2<f64>) {
-        self.prim.assign(rhs);
-    }
-
-    #[inline(always)]
     fn cons_entry(&self, j: usize, i: usize) -> f64 {
         return self.cons[[j, i]];
     }
@@ -73,46 +72,125 @@ impl<const S: usize> Physics for Euler1DIsot<S> {
     }
 
     #[inline(always)]
-    fn assign_cons(&mut self, rhs: &Array2<f64>) {
-        self.cons.assign(rhs);
+    fn eigen_vals(&self) -> ArrayView2<f64> {
+        return self.eigen_vals.view();
+    }
+
+    #[inline(always)]
+    fn eigen_min(&self) -> ArrayView1<f64> {
+        return self.eigen_vals.row(0);
+    }
+
+    #[inline(always)]
+    fn eigen_max(&self) -> ArrayView1<f64> {
+        return self.eigen_vals.row(1);
+    }
+
+    #[inline(always)]
+    fn c_sound(&self) -> ArrayView1<f64> {
+        return self.c_sound.view();
+    }
+
+    #[inline(always)]
+    fn flux_entry(&self, j: usize, i: usize) -> f64 {
+        return self.flux[[j, i]];
+    }
+
+    #[inline(always)]
+    fn flux_row(&self, j: usize) -> ArrayView1<f64> {
+        return self.flux.row(j);
+    }
+
+    #[inline(always)]
+    fn flux(&self) -> ArrayView2<f64> {
+        return self.flux.view();
     }
 
     fn update_prim(&mut self) {
         cons_to_prim(self.prim.view_mut(), self.cons.view());
     }
+
     fn update_cons(&mut self) {
         prim_to_cons(self.cons.view_mut(), self.prim.view());
     }
+
+    fn update_derived_values(&mut self) {
+        update_eigen_vals(self.eigen_vals.view_mut(), self.prim.row(1), self.c_sound.view());
+        update_flux(self.flux.view_mut(), self.prim.view(), self.cons.view(), self.c_sound.view());
+    }
+
+    #[inline(always)]
+    fn assign_prim_entry(&mut self, rhs: f64, j: usize, i: usize) {
+        self.prim[[j, i]] = rhs;
+    }
+
+    #[inline(always)]
+    fn assign_cons_entry(&mut self, rhs: f64, j: usize, i: usize) {
+        self.cons[[j, i]] = rhs;
+    }
+
+    #[inline(always)]
+    fn assign_prim(&mut self, rhs: &Array2<f64>) {
+        self.prim.assign(rhs);
+    }
+
+    #[inline(always)]
+    fn assign_cons(&mut self, rhs: &Array2<f64>) {
+        self.cons.assign(rhs);
+    }
+
 }
 
+/// Updates eigen values
+#[inline(always)]
+pub fn update_eigen_vals(mut eigen_vals: ArrayViewMut2<f64>, xi_vel: ArrayView1<f64>, c_sound: ArrayView1<f64>) {
+    Zip::from(eigen_vals.row_mut(0))
+        .and(xi_vel)
+        .and(c_sound)
+        .for_each(|e, &v, &cs| *e = v - cs);
+
+    Zip::from(eigen_vals.row_mut(1))
+        .and(xi_vel)
+        .and(c_sound)
+        .for_each(|e, &v, &cs| *e = v + cs);
+}
+
+/// Updates physical flux
+#[inline(always)]
+pub fn update_flux(mut flux: ArrayViewMut2<f64>, prim: ArrayView2<f64>, cons: ArrayView2<f64>, c_sound: ArrayView1<f64>) {
+    Zip::from(flux.row_mut(0))
+        .and(cons.row(1))
+        .for_each(|f, &xi_mom| *f = xi_mom);
+
+    Zip::from(flux.row_mut(1))
+        .and(cons.row(0))
+        .and(prim.row(1))
+        .and(c_sound)
+        .for_each(|f, &rho, &xi_vel, &cs| *f = rho * (xi_vel * xi_vel + cs * cs));
+}
+
+/// Converts conservative to primitive variables
 #[inline(always)]
 pub fn cons_to_prim(mut prim: ArrayViewMut2<f64>, cons: ArrayView2<f64>) {
         Zip::from(prim.row_mut(0))
             .and(cons.row(0))
-            .for_each(|rho_prim, &rho_cons| {
-                *rho_prim = rho_cons;
-            });
+            .for_each(|rho_prim, &rho_cons| *rho_prim = rho_cons);
 
         Zip::from(prim.row_mut(1))
             .and(cons.row(0))
             .and(cons.row(1))
-            .for_each(|xi_vel, &xi_mom, &rho_cons| {
-                *xi_vel = xi_mom / rho_cons;
-            });
+            .for_each(|xi_vel, &xi_mom, &rho_cons| *xi_vel = xi_mom / rho_cons);
 }
 
+/// Converts primitive to conservative variables
 #[inline(always)]
 pub fn prim_to_cons(mut cons: ArrayViewMut2<f64>, prim: ArrayView2<f64>) {
         Zip::from(cons.row_mut(0))
             .and(prim.row(0))
-            .for_each(|rho_cons, &rho_prim| {
-                *rho_cons = rho_prim;
-            });
+            .for_each(|rho_cons, &rho_prim| *rho_cons = rho_prim);
 
         Zip::from(cons.row_mut(1))
             .and(prim.row(0))
             .and(prim.row(1))
-            .for_each(|xi_mom, &xi_vel, &rho_cons| {
-                *xi_mom = xi_vel / rho_cons;
-            });
+            .for_each(|xi_mom, &xi_vel, &rho_cons| *xi_mom = xi_vel / rho_cons);
 }
