70732e5480af665dd818bb65c96cddef383ca3a5
tbreslein <tommy.breslein@protonmail.com>
2022-12-01 18:08:41 +0100

    remove old physics files

--- src/physics_old/systems.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022
-// Author: Tommy Breslein (github.com/tbreslein)
-// License: MIT
-
-//! Adds [super::Physics] implementations for different systems of differential equations
-
-pub mod euler1dadiabatic;
-pub mod euler1disot;
-pub mod euler2disot;
