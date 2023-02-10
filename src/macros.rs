26aa46657d4d16b07d2058f9dc035f5dfe41fa3d
tbreslein <tommy.breslein@protonmail.com>
2023-02-10 14:15:35 +0100

    add verbose doc strings and doc tests to the macros module

--- src/macros.rs
+++ src/macros.rs
@@ -8,6 +8,40 @@
 /// represents the number of equations in that system.
 /// This macro is used to make sure that E is always set correctly for your type of physics, while
 /// also giving you a useful type alias to make your code look more generic than it actually is.
+///
+/// NOTE: You need to have set the constant S, which sets how many cells the mesh will have, before
+/// calling this macro, otherwise it will not compile!
+///
+/// # Arguments
+///
+/// Only accepts either of:
+///
+/// * `Euler1DAdiabatic`
+/// * `Euler1DIsot`
+///
+/// # Examples
+///
+/// ```
+/// use corries::prelude::*;
+/// use std::any::TypeId;
+///
+/// // Set up adiabatic 1d Euler physics
+/// const S: usize = 100;
+/// set_Physics_and_E!(Euler1DAdiabatic);
+/// assert_eq!(TypeId::of::<P>(), TypeId::of::<Euler1DAdiabatic<S>>());
+/// assert_eq!(E, 3);
+/// ```
+///
+/// ```
+/// use corries::prelude::*;
+/// use std::any::TypeId;
+///
+/// // Set up isothermal 1d Euler physics
+/// const S: usize = 100;
+/// set_Physics_and_E!(Euler1DIsot);
+/// assert_eq!(TypeId::of::<P>(), TypeId::of::<Euler1DIsot<S>>());
+/// assert_eq!(E, 2);
+/// ```
 #[macro_export]
 macro_rules! set_Physics_and_E {
     (Euler1DAdiabatic) => {
@@ -17,13 +51,5 @@ macro_rules! set_Physics_and_E {
     (Euler1DIsot) => {
         type P = Euler1DIsot<S>;
         const E: usize = 2;
-    };
-    (Euler2DAdiabatic) => {
-        type P = Euler2DAdiabatic<S>;
-        const E: usize = 4;
-    };
-    (Euler2DIsot) => {
-        type P = Euler2DIsot<S>;
-        const E: usize = 3;
     };
 }
