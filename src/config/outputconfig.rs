56ef404d5ccf08089932875d88bb77bbc741a699
tbreslein <tommy.breslein@protonmail.com>
2023-02-10 12:45:40 +0100

    add a couple of default constructors for OutputConfig

--- src/config/outputconfig.rs
+++ src/config/outputconfig.rs
@@ -12,7 +12,7 @@ use color_eyre::eyre::ensure;
 use serde::Serialize;
 
 /// Carries information about how an `Output` is built
-#[derive(Debug, Serialize, Clone, Default)]
+#[derive(Debug, Serialize, Clone)]
 pub struct OutputConfig {
     /// Type of stream the `Output` writes to
     pub stream_mode: StreamMode,
@@ -43,6 +43,193 @@ pub struct OutputConfig {
 
     /// Identifiers for the data being written by the `Output`
     pub data_names: Vec<DataName>,
+}
+
+impl OutputConfig {
+    /// Constructs a OutputConfig default configuration that writes to stdout.
+    ///
+    /// This will set up the output such that the following values will be printed:
+    ///
+    /// `DataName::Iter, DataName::T, DataName::Dt, DataName::DtKind`
+    ///
+    /// Check out the asserts in the example to see what the other defaults are.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use corries::prelude::*;
+    ///
+    /// let stdout_config = OutputConfig::default_stdout();
+    ///
+    /// assert_eq!(stdout_config.stream_mode, StreamMode::Stdout);
+    /// assert_eq!(stdout_config.formatting_mode, FormattingMode::TSV);
+    /// assert_eq!(stdout_config.string_conversion_mode, ToStringConversionMode::Scalar);
+    /// assert_eq!(stdout_config.folder_name, "".to_string());
+    /// assert_eq!(stdout_config.should_clear_out_folder, false);
+    /// assert_eq!(stdout_config.file_name, "".to_string());
+    /// assert_eq!(stdout_config.precision, 3);
+    /// assert_eq!(stdout_config.should_print_ghostcells, false);
+    /// assert_eq!(stdout_config.should_print_metadata, false);
+    /// assert_eq!(stdout_config.data_names[0], DataName::Iter);
+    /// assert_eq!(stdout_config.data_names[1], DataName::T);
+    /// assert_eq!(stdout_config.data_names[2], DataName::Dt);
+    /// assert_eq!(stdout_config.data_names[3], DataName::DtKind);
+    /// ```
+    pub fn default_stdout() -> Self {
+        Self::default_stdout_with_names(vec![DataName::Iter, DataName::T, DataName::Dt, DataName::DtKind])
+    }
+
+    /// Constructs a OutputConfig default configuration that writes to stdout for a given set of
+    /// output values.
+    ///
+    /// Check out the asserts in the example to see what those defaults are.
+    ///
+    /// # Arguments
+    ///
+    /// * `data_names` - a vector of `DataName` objects that will be printed to stdout
+    ///
+    /// NOTE: `data_names` cannot contain identifiers for vector-like values, like
+    /// `DataName::XiCent`, because this configuration has `string_conversion_mode` set to
+    /// `ToStringConversionMode::Scalar`!
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use corries::prelude::*;
+    ///
+    /// let data_names = vec![DataName::Iter, DataName::T];
+    /// let stdout_config = OutputConfig::default_stdout_with_names(data_names);
+    ///
+    /// assert_eq!(stdout_config.stream_mode, StreamMode::Stdout);
+    /// assert_eq!(stdout_config.formatting_mode, FormattingMode::TSV);
+    /// assert_eq!(stdout_config.string_conversion_mode, ToStringConversionMode::Scalar);
+    /// assert_eq!(stdout_config.folder_name, "".to_string());
+    /// assert_eq!(stdout_config.should_clear_out_folder, false);
+    /// assert_eq!(stdout_config.file_name, "".to_string());
+    /// assert_eq!(stdout_config.precision, 3);
+    /// assert_eq!(stdout_config.should_print_ghostcells, false);
+    /// assert_eq!(stdout_config.should_print_metadata, false);
+    /// assert_eq!(stdout_config.data_names[0], DataName::Iter);
+    /// assert_eq!(stdout_config.data_names[1], DataName::T);
+    /// ```
+    pub fn default_stdout_with_names(data_names: Vec<DataName>) -> Self {
+        Self {
+            stream_mode: StreamMode::Stdout,
+            formatting_mode: FormattingMode::TSV,
+            string_conversion_mode: ToStringConversionMode::Scalar,
+            folder_name: "".to_string(),
+            should_clear_out_folder: false,
+            file_name: "".to_string(),
+            precision: 3,
+            should_print_ghostcells: false,
+            should_print_metadata: false,
+            data_names,
+        }
+    }
+
+    /// Constructs a OutputConfig default configuration that writes to files for a given set of
+    /// output values, folder path and file name.
+    ///
+    /// The files will be written to the path described by `folder_name`, and will be named
+    /// `<file_name>_XXX`, where `XXX` is the output count for that specific write. For example,
+    /// let's say `file_name` is set to `noh`, then the files will be named `noh_00.csv`,
+    /// `noh_01.csv`, `noh_02.csv`, and so on.
+    ///
+    /// Check out the asserts in the example to see what the defaults are.
+    ///
+    /// # Arguments
+    ///
+    /// * `folder_name` - A string slice describing the relative path to write the results to
+    /// * `file_name` - A string slice giving the base file name for the output files
+    /// * `num_equations` - Tells the function how many equations you are working with in order to
+    /// print all primitive variables
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use corries::prelude::*;
+    ///
+    /// let folder_name = "results";
+    /// let file_name = "noh";
+    /// let stdout_config = OutputConfig::default_file(folder_name, file_name, 3);
+    ///
+    /// assert_eq!(stdout_config.stream_mode, StreamMode::File);
+    /// assert_eq!(stdout_config.formatting_mode, FormattingMode::CSV);
+    /// assert_eq!(stdout_config.string_conversion_mode, ToStringConversionMode::Vector);
+    /// assert_eq!(stdout_config.folder_name, folder_name.to_string());
+    /// assert_eq!(stdout_config.should_clear_out_folder, true);
+    /// assert_eq!(stdout_config.file_name, file_name.to_string());
+    /// assert_eq!(stdout_config.precision, 7);
+    /// assert_eq!(stdout_config.should_print_ghostcells, true);
+    /// assert_eq!(stdout_config.should_print_metadata, true);
+    /// assert_eq!(stdout_config.data_names[0], DataName::T);
+    /// assert_eq!(stdout_config.data_names[1], DataName::XiCent);
+    /// assert_eq!(stdout_config.data_names[2], DataName::Prim(0));
+    /// assert_eq!(stdout_config.data_names[3], DataName::Prim(1));
+    /// assert_eq!(stdout_config.data_names[4], DataName::Prim(2));
+    /// ```
+    pub fn default_file(folder_name: &str, file_name: &str, num_equations: usize) -> Self {
+        let mut data_names = vec![DataName::T, DataName::XiCent];
+        for j in 0..num_equations {
+            data_names.push(DataName::Prim(j));
+        }
+        Self::default_file_with_names(folder_name, file_name, data_names)
+    }
+
+    /// Constructs a OutputConfig default configuration that writes to files for a given set of
+    /// output values, folder path and file name.
+    ///
+    /// The files will be written to the path described by `folder_name`, and will be named
+    /// `<file_name>_XXX`, where `XXX` is the output count for that specific write. For example,
+    /// let's say `file_name` is set to `noh`, then the files will be named `noh_00.csv`,
+    /// `noh_01.csv`, `noh_02.csv`, and so on.
+    ///
+    /// Check out the asserts in the example to see what the defaults are.
+    ///
+    /// # Arguments
+    ///
+    /// * `folder_name` - A string slice describing the relative path to write the results to
+    /// * `file_name` - A string slice giving the base file name for the output files
+    /// * `data_names` - a vector of `DataName` objects that will be printed to stdout
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use corries::prelude::*;
+    ///
+    /// let data_names = vec![DataName::Iter, DataName::T, DataName::Prim(0), DataName::Prim(1)];
+    /// let folder_name = "results";
+    /// let file_name = "noh";
+    /// let stdout_config = OutputConfig::default_file_with_names(folder_name, file_name, data_names);
+    ///
+    /// assert_eq!(stdout_config.stream_mode, StreamMode::File);
+    /// assert_eq!(stdout_config.formatting_mode, FormattingMode::CSV);
+    /// assert_eq!(stdout_config.string_conversion_mode, ToStringConversionMode::Vector);
+    /// assert_eq!(stdout_config.folder_name, folder_name.to_string());
+    /// assert_eq!(stdout_config.should_clear_out_folder, true);
+    /// assert_eq!(stdout_config.file_name, file_name.to_string());
+    /// assert_eq!(stdout_config.precision, 7);
+    /// assert_eq!(stdout_config.should_print_ghostcells, true);
+    /// assert_eq!(stdout_config.should_print_metadata, true);
+    /// assert_eq!(stdout_config.data_names[0], DataName::Iter);
+    /// assert_eq!(stdout_config.data_names[1], DataName::T);
+    /// assert_eq!(stdout_config.data_names[2], DataName::Prim(0));
+    /// assert_eq!(stdout_config.data_names[3], DataName::Prim(1));
+    /// ```
+    pub fn default_file_with_names(folder_name: &str, file_name: &str, data_names: Vec<DataName>) -> Self {
+        Self {
+            stream_mode: StreamMode::File,
+            formatting_mode: FormattingMode::CSV,
+            string_conversion_mode: ToStringConversionMode::Vector,
+            folder_name: folder_name.to_string(),
+            should_clear_out_folder: true,
+            file_name: file_name.to_string(),
+            precision: 7,
+            should_print_ghostcells: true,
+            should_print_metadata: true,
+            data_names,
+        }
+    }
 }
 
 unsafe impl Send for OutputConfig {}
